a28fece52f76bf563257e23f0f59995ce26703d9
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2012-07-18 14:20:47 -0400

    Moved output functions and Timer to module ARKWEB::Util

--- lib/arkweb-3.rb
+++ lib/arkweb-3.rb
@@ -8,17 +8,51 @@ require 'maruku'
 require 'wikicloth'
 require 'trollop'
 
-
-def msg(*args)
-  time = AW::Timer.time.to_s.ljust(5, '0')
-  puts "#{time} | #{args.join(' ')}"
-end
-def root(*args)
-  File.join(AW::Root, *args)
-end
-
-
 module ARKWEB
+
+
+
+# Utility functions and classes
+module Util
+
+  # Timer.time reports the time since the last call to Timer.reset
+  class Timer
+    def self.reset
+      @@start = Time.now
+    end
+    def self.time
+      t = Time.now - @@start
+      "#{t.round(2)}"
+    end
+    reset
+  end
+
+  # Write to standard output according to a standard format and verbosity
+  # options
+  def say(msg, sym='...', loud=false)
+    return false if Conf[:quiet]
+    return false if loud && !Conf[:verbose]
+    unless msg == ''
+      time = Timer.time.to_s.ljust(4, '0')
+      puts "#{time} #{sym} #{msg}"
+    else
+      puts
+    end
+  end
+  def msg(str)
+    say(str, '>>>', false)
+  end
+  def dbg(str)
+    say(str, '...', true)
+  end
+  def wrn(str)
+    say(str, '???', true)
+  end
+
+  def root(*args)
+    File.join(Root, *args)
+  end
+end
 
 
 
