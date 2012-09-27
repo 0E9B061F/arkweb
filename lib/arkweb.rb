31e9a458553a3ae3cbb37a56fff79151534452ce
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2012-09-27 08:06:18 -0400

    Split arkweb lib

--- lib/arkweb.rb
+++ lib/arkweb.rb
@@ -1,375 +1,105 @@
-# === ARKWEB-3 === #
+### ARKWEB
 
 require 'fileutils'
 require 'yaml'
 require 'erb'
+require 'pathname'
+require 'open-uri'
 
 require 'trollop'
+
+require_relative 'arkweb/utility'
+require_relative 'arkweb/error'
+require_relative 'arkweb/site'
+require_relative 'arkweb/engine'
+require_relative 'arkweb/interface'
+
+
+
+
+include ARKWEB::Utility
+
 
 
 module ARKWEB
 
+  Gem = {}
 
-Gem = {}
-
-
-# Utility functions and classes
-module Util
-
-  # Timer.time reports the time since the last call to Timer.reset
-  class Timer
-    def self.reset
-      @@start = Time.now
-    end
-    def self.time
-      t = Time.now - @@start
-      "#{t.round(2)}"
-    end
-    reset
-  end
-
-  # Write to standard output according to a standard format and verbosity
-  # options
-  def say(msg, sym='...', loud=false)
-    return false if Conf[:quiet]
-    return false if loud && !Conf[:verbose]
-    unless msg == ''
-      time = Timer.time.to_s.ljust(4, '0')
-      puts "#{time} #{sym} #{msg}"
-    else
-      puts
-    end
-  end
-  def msg(str)
-    say(str, '>>>', false)
-  end
-  def dbg(str)
-    say(str, '...', true)
-  end
-  def wrn(str)
-    say(str, '???', true)
-  end
-
-  def root(*args)
-    File.join(Root, *args)
-  end
-end
-
-
-
-class BrokenSiteError < StandardError
-end
-
-
-
-Root = File.absolute_path(File.join(File.dirname(__FILE__), '..'))
-ProjectFile = File.join(Root, 'project.yaml')
-FreezeFile  = File.join(Root, 'freeze.yaml')
-
-Project = YAML.load_file( File.exist?(FreezeFile) ? FreezeFile : ProjectFile )
-Pr = Project
-Version = "#{Pr['name']} v#{Pr['version']} codename '#{Pr['codename']}'"
-
-Usage = <<-BANNER
->>> #{Version}
-    Copyright 2012 nn <nn@studio25.org>
-#{Pr['name']} is a simple document processor for rendering flat websites.
-USAGE: ark [options] SITEPATH
-BANNER
-
-
-
-Conf = Trollop.options do
-  version Version
-  banner Usage
-  opt :output, "Directory to write output to",
-    :short => :o, 
-    :default => nil,
-    :type => :string
-  opt :verbose, "Toggle verbosity",
-    :short => :v
-  opt :quiet, "Run silently",
-    :short => :q
-# TODO
-#  opt :plugin, "A comma-seperated list of plugins to load",
-#    :short => :p,
-#    :default => nil
-end
-
-
-# TODO
-#class Plugin
-#end
-
-
-
-class Site
-
-  Path = {
-    :header   => "header.yaml",
-    :pages    => "*.page",
-    :page_erb => "page.html.erb",
-    :site_erb => "site.html.erb",
-    :output   => "html",
-    :sass     => "*.{scss,sass}",
-    :css      => "*.css",
-    :images   => "img"
-  }
-
-  Webfont = "http://fonts.googleapis.com/css?family="
-
-  def initialize(root)
-    @root = root
-    @name = File.basename(@root)
-    @path = self.make_path
-    FileUtils.mkdir_p(@path[:output])
-
-    @header = self.load_header
-    @author = @header['author']
-    @title  = @header['title']
-    @desc   = @header['desc'] || @header['description']
-    @tags   = @header['tags'] || @header['keywords']
-    @keywords = @tags ? @tags.join(', ') : ''
-
-    @styles = @header['styles']
-    if @header['webfonts']
-      webfonts = Webfont + @header['webfonts'].join('|')
-      @styles << webfonts
-    end
-
-    @files = {}
-    @files[:pages]  = Dir[@path[:pages]]
-    @files[:images] = Dir[@path[:images]]
-    @files[:css]    = Dir[@path[:css]]
-    @files[:sass]   = Dir[@path[:sass]]
-  end
-  attr_reader :root, :path, :title, :desc, :tags, :keywords, :author, :header
-  attr_reader :body, :styles, :files, :name
-  alias description desc
-
-  def site_template
-    @path[:site_erb]
-  end
-  def page_template
-    @path[:page_erb]
-  end
-
-  def make_path
-    path = {}
-    Path.each do |name, p|
-      path[name] = File.join(@root, p)
-    end
-    return path
-  end
-
-  def load_header
-    begin
-      YAML.load_file(self.path[:header])
-    rescue
-      raise BrokenSiteError,
-      "While loading site '#{self.root}': header file '#{@path[:header]}' is missing or malformed."
+  # Attempt to load an optional dependency
+  def self.optional_gem(name)
+    return Gem[name] unless Gem[name].nil?
+    Gem[name] = begin
+      require name
+      dbg "Loaded optional dependency: #{name}"
+      true
+    rescue LoadError
+      wrn "Unable to load optional gem '#{name}'; functionality provided by '#{name}' will be disabled."
+      false
     end
   end
 
-  def rel(path)
-    path[/^.*(#{@name}.*)$/, 1] || path
+
+  Root = File.absolute_path(File.join(File.dirname(__FILE__), '..'))
+  ProjectFile = File.join(Root, 'project.yaml')
+  FreezeFile  = File.join(Root, 'freeze.yaml')
+
+  Project = YAML.load_file( File.exist?(FreezeFile) ? FreezeFile : ProjectFile )
+  Pr = Project
+  Version = "#{Pr['name']} v#{Pr['version']} codename '#{Pr['codename']}'"
+
+  Usage = <<-BANNER
+  >>> #{Version}
+      Copyright 2012 nn <nn@studio25.org>
+  #{Pr['name']} is a simple document processor for rendering flat websites.
+
+  USAGE: ark [options] SITEPATH
+    where SITEPATH is a path to a valid site directory, or a nonexistant path. If
+    the path is nonexistant, a new site will be initialized at the given location.
+
+  Options:
+  BANNER
+
+
+
+  # TODO: move to interface
+  Conf = Trollop.options do
+    version Version
+    banner Usage
+    opt :output,
+        "Directory to write output to.",
+        :default => nil,
+        :type => :string
+    opt :verbose,
+        "Run verbosely. All messages will be reported."
+    opt :quiet,
+        "Run silently. Only fatal errors will be reported."
+    opt :clean,
+        "Remove temporary and cached files after running."
+    opt :clobber,
+        "Remove previously rendered files, temporary files and cache prior to running."
+    opt :validate,
+        "Validate rendered HTML with the W3C validator."
+    opt :minify,
+        "Minify CSS and Javascript files."
+  # TODO
+  #  opt :plugin, "A comma-seperated list of plugins to load",
+  #    :short => :p,
+  #    :default => nil
   end
 
-end
-
-
-
-class Sandbox
-  def initialize(env)
-    env.each do |k,v|
-      self.instance_variable_set("@#{k.to_s}", v)
-    end
+  if Conf[:minify]
+    require 'yui/compressor'
   end
-  def bindings
-    binding
-  end
-end
-
-
-
-class Engine
-
-  def initialize(site, mode='html5')
-    @site = site
-    @output = Conf[:output] || @site.path[:output]
-    FileUtils.mkdir_p(@output)
-    @page = ''
-    @body = ''
-    @pages = {}
-    @template = root("templates/#{mode}.html.erb")
-    @cache = {}
-  end
-  attr_reader :pages, :output
-
-  def read(file)
-    @cache[file] ||= File.open(file, 'r') {|f| f.read }
+  if Conf[:validate]
+    require 'w3c_validators'
   end
 
-  def evaluate_erb(file, env)
-    dbg "Evaluating ERB file: #{@site.rel(file)}"
-    data = self.read(file)
-    box = Sandbox.new(env)
-    erb = ERB.new(data)
-    erb.result(box.bindings)
-  end
 
-  def evaluate_md(file)
-    dbg "Evaluating Markdown file: #{@site.rel(file)}"
-    data = self.read(file)
-    doc = Maruku.new(data)
-    doc.to_html
-  end
-
-  def evaluate_wiki(file)
-    dbg "Evaluating MediaWiki markup file: #{@site.rel(file)}"
-    data = self.read(file)
-    doc = WikiCloth::Parser.new(:data => data)
-    doc.to_html
-  end
-
-  def render_page(page)
-    dbg "Rendering page: #{@site.rel(page)}"
-    type  = page[/\.(.+)\.page$/, 1]
-    @page = case type
-    when 'md'
-      unless Gem['maruku']
-        wrn "Skipped rendering Markdown page #{page}; please install gem 'maruku'"
-        return false
-      end
-      self.evaluate_md(page)
-    when 'wiki'
-      unless Gem['wikicloth']
-        wrn "Skipped rendering wiki page #{page}; please install gem 'wikicloth'"
-        return false
-      end
-      self.evaluate_wiki(page)
-    when 'erb'
-      self.evaluate_erb(page, :site => @site)
-    else
-      self.read(page)
-    end
-    @body = self.evaluate_erb(@site.page_template, :site => @site, :page => @page)
-    @page = ''
-    @pages[page] = if File.exist?(@site.site_template)
-      self.evaluate_erb(@site.site_template, :site => @site, :body => @body)
-    else
-      self.evaluate_erb(@template, :site => @site, :body => @body)
-    end
-    @body = ''
-    return true
-  end
-
-  def copy_resources
-    unless @site.files[:images].empty?
-      dbg "Copying image directory: #{@site.rel(@site.path[:images])} -> #{@site.rel(@output)}"
-      FileUtils.cp_r(@site.path[:images], @output)
-    end
-
-    unless @site.files[:css].empty?
-      dbg "Copying style sheets: #{@site.files[:css].join(', ')} -> #{@site.rel(@output)}"
-      FileUtils.cp_r(@site.files[:css], @output)
-    end
-
-    # Run sass over sass files
-    @site.files[:sass].each do |sass|
-      css = File.basename(sass).sub(/\.[^\.]+$/, '.css')
-      css = File.join(@output, css)
-
-      # Only render if output doesn't already exist, or if output is outdated
-      if !File.exist?(css) || File.mtime(sass) > File.mtime(css)
-        dbg "Rendering SASS file '#{@site.rel(sass)}' to '#{@site.rel(css)}'"
-        `sass -t compressed #{sass} #{css}`
-      end
-    end
-  end
-
-  def write_page(page)
-    msg "Writing page: #{@site.rel(page)}"
-    base = File.basename(page)
-    name = base[/(.+)\..+?\.page$/, 1]
-    out = File.join(@output, "#{name}.html")
-    unless @pages[page]
-      if self.render_page(page)
-        File.open(out, 'w') {|f| f.write(@pages[page]) }
-      end
-    end
-  end
-
-  def write_site
-    # self.run_before_hook
-    @site.files[:pages].each do |page|
-      self.write_page(page)
-    end
-    self.copy_resources
-    # self.run_after_hook
-  end
-
-end
-
-
-
-class Interface
-
-# TODO
-#  def self.load_plugins
-#    if Conf[:plugin]
-#      plugins = Conf[:plugin].split(',')
-#      plugins.each do |plugin|
-#        name = File.basename(plugin).sub(/\.[^\.]+?$/,'').capitalize
-#        load plugin
-#        klass = eval "ARKWEB::#{name}"
-#        ARKWEB.register_plugin(klass)
-#      end
-#    end
-#  end
-
-  def self.run
-    msg Version
-    path = ARGV[0]
-    if path && File.directory?(path)
-      msg "Processing site: #{path}"
-      site = Site.new(path)
-      eng  = Engine.new(site)
-      eng.write_site
-      msg "Done! Wrote site to: #{eng.output}"
-    elsif path && !File.exist?(path)
-      msg "Initializing site: #{path}"
-      FileUtils.mkdir_p(path)
-      target = root('skel', '*')
-      FileUtils.cp_r(Dir[target], path)
-      msg "Done! Initialized site: #{path}"
-    else
-      wrn "Please supply a path to a site directory"
-    end
-  end
-
-end
+  # TODO
+  #class Plugin
+  #end
 
 
 
 end # module ARKWEB
-
-
-
-AW = ARKWEB
-include AW::Util
-
-AW::Gem['maruku'] = begin
-  require 'maruku'
-rescue LoadError
-  wrn "Unable to load gem 'maruku'; markdown rendering will be disabled."
-  false
-end
-
-AW::Gem['wikicloth'] = begin
-  require 'wikicloth'
-rescue LoadError
-  wrn "Unable to load gem 'wikicloth'; wiki markup rendering will be disabled."
-  false
-end
 
