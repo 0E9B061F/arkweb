b9b9787898c41a1ffa5ee506b25e3fbf36fa04c7
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2015-09-01 19:08:15 -0400

    Added Stylesheet class

--- lib/arkweb/site.rb
+++ lib/arkweb/site.rb
@@ -102,7 +102,68 @@ class Page
 	end
 end
 
+# This class represents a stylesheet, in CSS or one of the flavors of SASS. The
+# stylesheet may be a site-wide style found in the ARKWEB directory, or a
+# section-specific style found in the site structure.
+class Stylesheet
+	def initialize(site, working_path, section=nil)
+		# Relations
+		@site    = site
+		@section = section
+
+		# Path stuff
+		# working_path: path to the file relative to the current working directory
+		# site_path:    path relative to the site root
+		# output_path:  path to the output location, relative to the current working directory
+		# server_path:  path to the file when the output is served as a website; as if the site root were the filesystem root
+		@working_path  = working_path
+		@basename      = File.basename(@working_path)
+		@name          = @basename[/^[^\.]+/]
+		@extension     = @basename[/\..+$/]
+		@rendered_name = "#{@name}.css"
+
+		# The site_path isn't wholly necessary for site styles but we'll keep in either case, for consistency
+		@site_path    = Pathname.new(@working_path).relative_path_from(Pathname.new(@site.root)).to_s
+
+		if self.site_style?
+			@output_path  = File.join(@site[:aw_out], @rendered_name)
+			@aw_path      = Pathname.new(@output_path).relative_path_from(Pathname.new(@site[:output])).to_s
+			@server_path  = File.join('/', @aw_path)
+		else
+			site_dirname  = File.dirname(@site_path)
+			rendered_path = File.join(site_dirname, @rendered_name)
+			@output_path  = File.join(@site[:output], rendered_path)
+			@server_path  = File.join('/', rendered_path)
+		end
+	end
+
+	# True if this stylesheet is found in the ARKWEB directory. False if located
+	# in the site structure.
+	def site_style?
+		return @section.nil?
+	end
+
+	# Return true if this stylesheet is in SASS
+	def is_sass?
+		return not @basename[/\.s[ca]ss$/].nil?
+	end
+
+	# Represent this object as the working path to the given stylesheet
+	def to_s()
+		return @working_path
+	end
+
+	# Return a link to this stylesheet
+	def head_link()
+		return %Q(<link href="#{@server_path}" rel="stylesheet" type="text/css" />)
+	end
+end
+
+
 class Site
+
+	RootSectionName = 'root'
+	OutputARKWEB    = 'aw'
 
   Paths = {}
 	Paths[:arkweb]   = "ARKWEB"
@@ -111,7 +172,7 @@ class Site
 	Paths[:site_erb] = File.join(Paths[:arkweb], "site.html.erb")
 	Paths[:style]    = File.join(Paths[:arkweb], "site.{css,sass,scss}")
 	Paths[:output]   = File.join(Paths[:arkweb], "output")
-	Paths[:aw_out]   = File.join(Paths[:output], "aw")
+	Paths[:aw_out]   = File.join(Paths[:output], OutputARKWEB)
 	Paths[:img_out]  = File.join(Paths[:aw_out], "images")
 	Paths[:tmp]      = File.join(Paths[:arkweb], "tmp")
 	Paths[:cache]    = File.join(Paths[:arkweb], "cache")
@@ -131,12 +192,9 @@ class Site
       return [url + fonts]
     },
     :fontsquirrel => lambda {|fonts|
-			# XXX this /aw/ shouldn't be hardcoded here but it is for now
-      fonts.map {|font| "/aw/#{font}.css" }
+      fonts.map {|font| File.join('/', OutputARKWEB, "#{font}.css") }
     }
   }
-
-	RootSectionName = 'root'
   
   def initialize(root)
     raise BrokenSiteError unless File.directory?(root)
